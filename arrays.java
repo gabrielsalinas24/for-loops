bb9716004413aa588a9888ed99d9e8ec29f93ce4
gabrielsalinas24 <113557530+gabrielsalinas24@users.noreply.github.com>
2022-09-29 14:17:31 -0700

    Add files via upload

---
+++ arrays.java
@@ -0,0 +1,13 @@
+public class arrays
+{
+    public static void main(String[] args) {
+        String[] strArray = {"Alpha", "Bravo", "Charlie", "Delta", "Echo"};
+        for (int i = 0, myArraylength;i++){
+            myArray[i]=myArray[i]*2;
+        }
+        system.out.printls("Alpha");
+        system.out.printls("Bravo");
+        system.out.printls("Charlie");
+        system.out.printls("Delta");
+        system.out.printls("Echo");
+}
